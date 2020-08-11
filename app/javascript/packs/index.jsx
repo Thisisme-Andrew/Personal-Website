c1b0b1d7ab6e522fcad79bc5ac86b3414e1c3319
Thisisme-Andrew <andy.tangg4@gmail.com>
2020-08-11 13:38:43 -0600

    init

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,17 +6,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App/>,
