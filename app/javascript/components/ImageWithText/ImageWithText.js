a4bd549e6d287d7b78e5d48c917a91dabd22b06d
Thisisme-Andrew <andy.tangg4@gmail.com>
2020-08-11 22:24:23 -0600

    css fix7

--- app/javascript/components/ImageWithText/ImageWithText.js
+++ app/javascript/components/ImageWithText/ImageWithText.js
@@ -6,9 +6,14 @@ class ImageWithText extends React.Component {
     render () {
         return (
             <div className = "Profile">
-                <img src = {Me} className = "Me"/>
-                <h2 className = "bioH2">This is me: Andrew T</h2>
-                <p className = "bioP">I wanted to create a website that would allow me to great flash cards and crap</p>
+                <img src = {Me} className = "image"/>
+                <div className = "text">
+                    <h2 className = "bioH2">This is me: Andrew T</h2>
+                    <p className = "bioP">I wanted to create a website that would allow me to great flash cards and crap</p>
+                    <form action="https://google.com">
+                        <input type="submit" value="More on Me" />
+                    </form>
+                </div>
             </div>
         )
     }
