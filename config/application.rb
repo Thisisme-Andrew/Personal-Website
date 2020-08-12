05aa814a83650cf5af18660c49de2d1fd7ec5035
Thisisme-Andrew <andy.tangg4@gmail.com>
2020-08-11 21:04:41 -0600

    css fix5

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.paths << Rails.root.join("node_modules")
+
 module ReactApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
